48bb54a35e9d15d8e83f2767fbdf16233248c559
Zen Kun <zenyagami@gmail.com>
2016-06-18 10:53:11 -0500

    picasso working for loading images

--- Clase1/app/src/main/java/com/unam/clase1/adapters/AdapterItemList.java
+++ Clase1/app/src/main/java/com/unam/clase1/adapters/AdapterItemList.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.unam.clase1.R;
 import com.unam.clase1.model.ModelItem;
 
@@ -17,6 +18,8 @@ import java.util.List;
  * Created by hacke on 11/06/2016.
  */
 public class AdapterItemList extends ArrayAdapter<ModelItem> {
+    private final String url1="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Biblioteca_Central_UNAM_M%C3%A9xico.jpg/250px-Biblioteca_Central_UNAM_M%C3%A9xico.jpg";
+    private final String url2="https://www.unam.mx/sites/default/files/images/menu/library-345273_1280.jpg";
     public AdapterItemList(Context context,List<ModelItem> objects) {
         super(context, 0, objects);
 
@@ -31,11 +34,13 @@ public class AdapterItemList extends ArrayAdapter<ModelItem> {
         TextView txtItemDescription= (TextView) convertView.findViewById(R.id.txtItemDescription);
         TextView txtTitle = (TextView) convertView.findViewById(R.id.txtItemTitle);
         ImageView img = (ImageView) convertView.findViewById(R.id.row_image_view);
-
         ModelItem modelItem=getItem(position);
+        Picasso.with(getContext()).load(modelItem.resourceId==R.drawable.ic_device_signal_wifi_4_bar?
+                url1:url2).into(img);
+
         txtTitle.setText(modelItem.item);
         txtItemDescription.setText(modelItem.description);
-        img.setImageResource(modelItem.resourceId);
+        //img.setImageResource(modelItem.resourceId);
         return convertView;
     }
 }
