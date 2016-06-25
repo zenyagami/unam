7668a617db19e23f9ad67e0e4aaf71ce64629d37
Zen Kun <zenyagami@gmail.com>
2016-06-24 21:11:51 -0500

    working staractivityForResult

--- Notificaciones/app/src/main/java/unam/com/notificaciones/Main2Activity.java
+++ Notificaciones/app/src/main/java/unam/com/notificaciones/Main2Activity.java
@@ -1,11 +1,13 @@
 package unam.com.notificaciones;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -13,17 +15,21 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final EditText name = (EditText) findViewById(R.id.etName);
+        final EditText desc= (EditText) findViewById(R.id.etDesc);
+        findViewById(R.id.btnSave).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.putExtra("key_name",name.getText().toString());
+                i.putExtra("key_desc",desc.getText().toString());
+                setResult(RESULT_OK,i);
+                finish();
+
             }
         });
+
+
     }
 
 }
--- Notificaciones/app/src/main/java/unam/com/notificaciones/MainActivity.java
+++ Notificaciones/app/src/main/java/unam/com/notificaciones/MainActivity.java
@@ -5,20 +5,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import unam.com.notificaciones.service.ServiceNotification;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int REQUEST_CODE_SECOND_ACTIVITY = 1;
+    private TextView txtName;
+    private TextView txtDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btnNotif).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        findViewById(R.id.btnNotif).setOnClickListener(this);
+        findViewById(R.id.btnEdit).setOnClickListener(this);
+        txtName = (TextView)findViewById(R.id.txtNombre);
+        txtDesc = (TextView)findViewById(R.id.txtDescription);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.btnNotif:
                 startService(new Intent(getApplicationContext(), ServiceNotification.class));
-            }
-        });
+                break;
+            case R.id.btnEdit:
+                startActivityForResult(
+                        new Intent(getApplicationContext(),Main2Activity.class)
+                        ,REQUEST_CODE_SECOND_ACTIVITY);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(REQUEST_CODE_SECOND_ACTIVITY==requestCode && resultCode==RESULT_OK)
+        {
+            String name = data.getExtras().getString("key_name","");
+            String desc = data.getExtras().getString("key_desc","");
+            txtName.setText(name);
+            txtDesc.setText(desc);
+        }else
+            super.onActivityResult(requestCode, resultCode, data);
     }
 }
