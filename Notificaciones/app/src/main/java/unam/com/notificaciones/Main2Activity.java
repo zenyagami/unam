e636f60919cb032f8d9fff8a9895124e14d836a6
Zen Kun <zenyagami@gmail.com>
2016-06-25 10:52:09 -0500

    toolbar working

--- Notificaciones/app/src/main/java/unam/com/notificaciones/Main2Activity.java
+++ Notificaciones/app/src/main/java/unam/com/notificaciones/Main2Activity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -28,8 +30,28 @@ public class Main2Activity extends AppCompatActivity {
 
             }
         });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Hola mundo2");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
